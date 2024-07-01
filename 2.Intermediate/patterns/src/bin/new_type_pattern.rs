07a2eccbd71132b39d6d0a71e68036a51690099b
Abdurraham Pektas <abdurraham.pektas@afiniti.com>
2024-07-01 16:41:29 +0300

    Strong typing using new type pattern

--- 2.Intermediate/patterns/src/bin/new_type_pattern.rs
+++ 2.Intermediate/patterns/src/bin/new_type_pattern.rs
@@ -23,11 +23,28 @@ impl Inches {
     }
 }
 
+// Example: Strong Typing with Newtype
+struct UserId(u32);
+struct OrderId(u32);
 
+fn find_user_by_id(id: UserId) {
+    // function implementation
+}
+
+fn find_order_by_id(id: OrderId) {
+    // function implementation
+}
 fn main() {
 
     let length = Inches::new(10);
     println!("Length in inches: {}", length.value());
     println!("Length using deref: {}", *length); // Deref allows us to access the inner value directly
 
+    // In this example, UserId and OrderId are both based on u32,
+    // but they are distinct types and cannot be interchanged, providing stronger type safety.
+    let user_id = UserId(42);
+    let order_id = OrderId(99);
+
+    find_user_by_id(user_id);
+    find_order_by_id(order_id);
 }
